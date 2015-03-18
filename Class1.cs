c15bdab18fdcfff142b8f0ddf520c230ea920df2
danjocutler <danjocutler@gmail.com>
2015-03-18 20:37:22 +0000

    Divisible by 3 and 5 tests passing

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FizzBuzzCSharp
+{
+
+    public class Fizzbuzz
+    {
+        [STAThread]
+        static void Main()
+        { 
+        }
+
+        public bool DivisibleByThree(int number)
+        {
+            if (number % 3 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool DivisibleByFive(int number)
+        {
+            if (number % 5 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Test1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace FizzBuzzCSharp
+{
+    /// <summary>
+    /// A test class for confirming the functionality of the Fizzbuzz methods
+    /// </summary>
+    [TestFixture]
+    public class FizzbuzzTest
+    {
+
+        Fizzbuzz fizzbuzz;
+
+        [SetUp]
+        public void SetupForEachTest()
+        {
+            fizzbuzz = new Fizzbuzz();
+        }
+
+        [Test]
+        public void DivisibleByThree()
+        {
+            fizzbuzz.DivisibleByThree(3);
+  
+            Assert.IsTrue(true);
+
+            fizzbuzz.DivisibleByThree(1);
+
+            Assert.IsFalse(false);
+        }
+
+        [Test]
+        public void DivisibleByFive()
+        {
+            fizzbuzz.DivisibleByFive(5);
+
+            Assert.IsTrue(true);
+
+            fizzbuzz.DivisibleByFive(1);
+
+            Assert.IsFalse(false);
+        
+        }
+    }
+}
